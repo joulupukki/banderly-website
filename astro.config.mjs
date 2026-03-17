ee1505936caaf9d922a281a9a3c0c11a3c5f04d2
Boyd Timothy <btimothy@gmail.com>
2026-03-17 12:38:17 -0600

    Switch custom domain to www.banderly.com for apex redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://banderly.com',
+  site: 'https://www.banderly.com',
   output: 'static',
 });
